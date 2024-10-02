097f60567c1bea55f39246fc77a0c5f7a0912292
OtmaneTouhami <otmanetouhami01@gmail.com>
2024-10-02 15:35:11 +0100

    alx-files_manager

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,7 @@
 import mongodb from 'mongodb';
 // eslint-disable-next-line no-unused-vars
 import Collection from 'mongodb/lib/collection';
+import envLoader from './env_loader';
 
 /**
  * Represents a MongoDB client.
@@ -10,6 +11,7 @@ class DBClient {
    * Creates a new DBClient instance.
    */
   constructor() {
+    envLoader();
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
